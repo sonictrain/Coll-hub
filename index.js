a935f1882871f8b13263e58d3b57aced39f3167c
sonictrain <nicola.brucoli92@gmail.com>
2024-02-14 18:28:34 +0000

    moving prompts.js file from lib/ to src/

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-const { startingPrompts, nextPrompts } = require("./lib/prompts")
+const { startingPrompts, nextPrompts } = require("./src/prompts")
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
